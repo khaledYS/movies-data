c920ad6ecd5a07d71375684c80bbbe750739cd75
khaledYS <73815164+khaledYS@users.noreply.github.com>
2021-05-07 02:09:06 +0300

    moviesdata

---
+++ moviesData.js
@@ -0,0 +1,14 @@
+export var bgData = `
+{
+    "ALAA ALDEEN" : ["images/detective-conan-1/images/detective-conan-1.png", "ALAA ALDEEN", ""],
+    "DEADPOOL 2" : ["images/deadpool/deadpool-pic.png", "Deadpool 2", ""],
+    "DETECTIVE CONAN 1" : ["images/detective-conan-1/images/detective-conan-1.png", "Detective Conan 1", ""],
+    "MASTER" : ["images/detective-conan-1/images/detective-conan-1.png", "MASTER", ""],
+    "MONSTER HUNTER" : ["images/detective-conan-1/images/detective-conan-1.png", "MONSTER HUNTER", ""],
+    "MR ROBOT" : ["images/detective-conan-1/images/detective-conan-1.png", "MR ROBOT", ""],
+    "OUTSIDE THE WIRE" : ["images/detective-conan-1/images/detective-conan-1.png", "OUTSIDE THE WIRE", ""],
+    "PAPER LIVES" : ["images/detective-conan-1/images/detective-conan-1.png", "PAPER LIVES", ""],
+    "RUN" : ["images/detective-conan-1/images/detective-conan-1.png", "RUN", ""],
+    "The Mitchells vs. the Machines" : ["images/detective-conan-1/images/detective-conan-1.png", "The Mitchells vs. the Machines", ""]
+}
+`;
